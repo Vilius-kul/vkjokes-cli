9ba3f6c5f7bf6b3afe2d579f884625ca8ed51b9d
vilius <vilius.kulinas@gmail.com>
2021-09-29 16:50:08 +0100

    Working CLI

--- vkjokes.py
+++ vkjokes.py
@@ -1,25 +1,50 @@
+import requests
+
 import click
+
+base_url = 'http://127.0.0.1:5000/'
+
 
 @click.group()
 def cli():
-    """Welcome to the jokes CLI!"""
+    """Welcome to the jokes CLI!\n
+        This script returns jokes!
+    """
     pass
 
 @click.command()
 def random_joke():
-    click.echo("1 randm joke!")
+    endpoint = 'random-joke'
+    url = base_url + endpoint
+    r = requests.get(url)
+    click.echo(r.text)
     
 @click.command()
 def random_5_jokes():
-    click.echo("5 random jokes!")
+    endpoint = 'random-5-jokes'
+    url = base_url + endpoint
+    r = requests.get(url)
+    click.echo(r.text)
     
 @click.command()
-def multi_random_jokes():
-    click.echo("Multiple random jokess!")
+@click.option('--count', default=1, help='type --count <number of jokes>')
+def multi_random_jokes(count):
+    endpoint = 'multi-random-jokes'
+    url = base_url + endpoint
+    payload = {'count': count}
+    r = requests.get(url, params=payload)
+    click.echo(r.text)
+    
     
 @click.command()
-def multi_language_jokes():
-    click.echo("Multiple language jokes!")
+@click.option('--count', default=1, help="--count <number of jokes>")
+@click.argument('lang')
+def multi_language_jokes(count, lang):
+    endpoint = 'multi-language-jokes'
+    url = base_url + endpoint
+    payload = {'count': count,'language':lang}
+    r = requests.get(url, params=payload)
+    click.echo(r.text)
     
 cli.add_command(random_joke)
 cli.add_command(random_5_jokes)
